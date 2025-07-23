fcd2284e23e7b0b62b52aed9e384978e2a706526
Shazam <shazajmal37@gmail.com>
2025-07-23 14:06:43 +0530

    added postgre DB hosted on render

--- database.py
+++ database.py
@@ -2,10 +2,16 @@ from sqlalchemy import create_engine, Column, String, Date, DateTime, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from datetime import datetime
+import os
 
 # Database setup
-SQLALCHEMY_DATABASE_URL = "sqlite:///./documents.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+database_url = os.environ.get('DATABASE_URL')
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./documents.db"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
